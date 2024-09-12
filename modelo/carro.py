17c8e095e174419eedd9d73ef4873f9b712620b2
nando244 <134401180+nando244@users.noreply.github.com>
2024-09-12 12:11:20 -0300

    Add files via upload

---
+++ modelo/carro.py
@@ -0,0 +1,16 @@
+class Viagem:
+    def__int__(self,)
+    nome = ""
+    pais_Origem = ""
+    pais_Destino = ""
+    ativo = False
+
+pessoa_Vinicius = Viagem()
+pessoa_Luiza = Viagem()
+
+pessoa_Luiza.nome = "Luiza Ribeiro"
+pessoa_Luiza.pais_Origem = "Brasil"
+pessoa_Luiza.pais_Destino = "Alemanha"
+
+Viagens = [pessoa_Luiza, pessoa_Vinicius]
+
