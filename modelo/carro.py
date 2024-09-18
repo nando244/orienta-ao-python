1f0bbc74c5051dd7f33ce4e1781d8317f71b9c54
nando244 <134401180+nando244@users.noreply.github.com>
2024-09-18 09:16:38 -0300

    Add files via upload

--- modelo/carro.py
+++ modelo/carro.py
@@ -1,21 +1,50 @@
-class Carro:
-clientes = []
+class Carros:
+    carros = []
 
-def __init__(self, nome, servico, horario, pagamento):
-self.nome = nome
-self.servico = servico
-self.horario = horario
-self.pagamento = pagamento
-Cliente.clientes.append(self)
+    def __init__(self, modelo, fabricante, cor, ano):
+        self.modelo = modelo
+        self.fabricante = fabricante
+        self.cor = cor
+        self.ano = ano
+        self._disponivel = False
+        Carros.carros.append(self)
 
-def __str_(self):
-return f'{self.nome} | {self.servico} | {self.horario} | {self.pagamento}'
+    def __str__(self):
+        return f'{self.modelo} | {self.fabricante} | {self.cor} | {self.ano} | {self.disponivel}'
 
-def listar_cliente():
-for cliente in Cliente.clientes:
-print(f'{cliente.nome} | {cliente.servico} | {cliente.horario} | {cliente.pagamento}')
-cliente_Maria = Cliente('Maria', 'Corte de cabelo', '10:00', 'Dinheiro')
-cliente_Fernanda = Cliente('Fernanda', 'Manicure', '14:00', 'Cartão')
-cliente_Ana = Cliente('Ana', 'Corte de cabelo', '11:00', 'Dinheiro')
+    @staticmethod
+    def listar_carros():
+        print('')
+        print('''
+░█████╗░░█████╗░██████╗░██████╗░░█████╗░░██████╗
+██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝
+██║░░╚═╝███████║██████╔╝██████╔╝██║░░██║╚█████╗░
+██║░░██╗██╔══██║██╔══██╗██╔══██╗██║░░██║░╚═══██╗
+╚█████╔╝██║░░██║██║░░██║██║░░██║╚█████╔╝██████╔╝
+░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═════╝░
 
-Cliente.listar_cliente()
+██████╗░██╗░██████╗██████╗░░█████╗░███╗░░██╗██╗██╗░░░██╗███████╗██╗░██████╗
+██╔══██╗██║██╔════╝██╔══██╗██╔══██╗████╗░██║██║██║░░░██║██╔════╝██║██╔════╝
+██║░░██║██║╚█████╗░██████╔╝██║░░██║██╔██╗██║██║╚██╗░██╔╝█████╗░░██║╚█████╗░
+██║░░██║██║░╚═══██╗██╔═══╝░██║░░██║██║╚████║██║░╚████╔╝░██╔══╝░░██║░╚═══██╗
+██████╔╝██║██████╔╝██║░░░░░╚█████╔╝██║░╚███║██║░░╚██╔╝░░███████╗██║██████╔╝
+╚═════╝░╚═╝╚═════╝░╚═╝░░░░░░╚════╝░╚═╝░░╚══╝╚═╝░░░╚═╝░░░╚══════╝╚═╝╚═════╝░
+''')
+
+        print(f'{"Modelo".ljust(20)} | {"Fabricante".ljust(20)} | {"Cor".ljust(20)} | {"Ano".ljust(20)} | {"Disponível"}')
+        print('------------------------------------------------------------------------------------------------------------------------------')
+        for carro in Carros.carros:
+            print(f'{carro.modelo.ljust(20)} | {carro.fabricante.ljust(20)} | {carro.cor.ljust(20)} | {carro.ano.ljust(20)} | {carro.disponivel}')
+            print('-------------------------------------------------------------------------------------------------------------------------')
+
+    @property
+    def disponivel(self):
+        return '❌ Indisponível' if self._disponivel else '✔️ Disponível'        
+
+# Exemplos de carros
+carro_fusca = Carros('Fusca', 'Volkswagen', 'Azul', '1970')
+carro_civic = Carros('Civic', 'Honda', 'Preto', '2020')
+carro_jetta = Carros('Jetta', 'Volkswagen', 'Branco', '2019')
+
+# Listar carros
+Carros.listar_carros()
